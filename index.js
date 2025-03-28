94213d80b0e82ac164eee26fe2c763000c2273a6
BRUNO NAHUEL MARASI <48113202@ort>
2025-03-28 10:09:55 -0300

    terminamos2

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ let arrayValores = [1, 5, "hola", "flecha"]
 
 const mostrarValoresArray = () => {
     console.log("PUNTO 2")
-    arrayValores.forEach(p => {
-        console.log(p)
-    })
+
+    console.log(`El array es: ${arrayValores}`)
+
     console.log("")
 }
 mostrarValoresArray()
@@ -19,9 +19,8 @@ const mostrarValorAgregado = () => {
     console.log("PUNTO 3")
     arrayValores.push("salvador soncini")
 
-    arrayValores.forEach(p => {
-        console.log(p)
-    })
+    console.log(`El array es: ${arrayValores}`)
+
     console.log("")
 }
 
@@ -31,7 +30,7 @@ mostrarValorAgregado()
 const encontrarUltimaPosicion = () => {
     console.log("PUNTO 4")
     const ultimaPosicion = arrayValores.lastIndexOf("salvador soncini")
-    console.log(ultimaPosicion)
+    console.log(`La ultima posicion es: ${ultimaPosicion}`)
     console.log("")
 }
 encontrarUltimaPosicion()
@@ -39,12 +38,12 @@ encontrarUltimaPosicion()
 //5
 const sumaArrayNumeros = () => {
     console.log("PUNTO 5")
-    let arrayNumeros = [1, 2, 3, 4, 5]
+    const arrayNumeros = [1, 2, 3, 4, 5]
     let suma = 0
-    arrayNumeros.forEach(n => {
-        suma += n
-    })
-    console.log(suma)
+    
+    suma = arrayNumeros.reduce((a,b) => {return a+b})
+
+    console.log(`La suma de todo el array es: ${suma}`)
     console.log("")
 }
 sumaArrayNumeros()
@@ -53,9 +52,9 @@ sumaArrayNumeros()
 const invertirOrdenArray = () => {
     console.log("PUNTO 6")
     const arrayInvertido = arrayValores.reverse()
-    arrayInvertido.forEach(p => {
-        console.log(p)
-    })
+    
+    console.log(`El array invertido es: ${arrayInvertido}`)
+
     console.log("")
 }
 invertirOrdenArray()
@@ -65,7 +64,7 @@ const ordenarArrayMenorAMayor = () => {
     console.log("PUNTO 7")
     let arrayAOrdenar = [5, 8, 2, 4, 6]
     const arrayOrdenado = arrayAOrdenar.sort((a, b) => a - b)
-    console.log(arrayOrdenado)
+    console.log(`El array ordenado es: ${arrayOrdenado}`)
     console.log("")
 }
 ordenarArrayMenorAMayor()
@@ -75,7 +74,7 @@ const filtrarNumerosMayoresA10 = () => {
     console.log("PUNTO 8")
     let valoresAFiltrar = [20, 5, 800, 58, 69, 4, 6, 3, 8]
     const valoresFiltrados = valoresAFiltrar.filter(n => n > 10)
-    console.log(valoresFiltrados)
+    console.log(`Los valores filtrados son: ${valoresFiltrados}`)
     console.log("")
 }
 filtrarNumerosMayoresA10()
@@ -84,8 +83,8 @@ filtrarNumerosMayoresA10()
 const contarElementos = () => {
     console.log("PUNTO 9")
     let valoresContar = [1, 5, 3, 3, 7, 3, 5, 6, 7, 8, 2, 1]
-    const valoresContados = valoresContar.filter(v => v === 7).length
-    console.log(valoresContados)
+    const valoresContados = valoresContar.filter(v => v === 3).length
+    console.log(`El numero 3 se repite: ${valoresContados} veces`)
     console.log("")
 }
 contarElementos()
